9b1b6179746ac56ccc530488ed6f8004efcf7f7e
sugiyem <farrelds2003@gmail.com>
2021-12-28 19:14:32 +0800

    1st commit

--- app/models/advanced_search.rb
+++ app/models/advanced_search.rb
@@ -3,11 +3,11 @@ class AdvancedSearch < ApplicationRecord
         tasks = Task.all
         
         tasks = tasks.where(["lower(task_name) LIKE lower(?)", "%#{keywords}%"]) if keywords.present?
-        tasks = tasks.where(["tag LIKE ?", "%#{tag}%"]) if tag.present?
+        tasks = tasks.where(["lower(tag) LIKE lower(?)", "%#{tag}%"]) if tag.present?
         tasks = tasks.where(["deadline >= ?", "%#{min_date}%"]) if min_date.present?
         tasks = tasks.where(["deadline <= ?", "%#{max_date}%"]) if max_date.present?
         tasks = tasks.where(["is_done LIKE ?", "%#{status}%"]) if status.present?
-        tasks = tasks.sort_by &:deadline
+        tasks = tasks.sort_by {|t| [t.is_done ? 0 : 1, t.deadline]}
 
         return tasks
     end
